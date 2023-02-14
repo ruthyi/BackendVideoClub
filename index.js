b097dae0cab745280aaf5e1c607502187e567443
ruthyi <jbruthlizcano@gmail.com>
2023-02-14 17:08:00 -0500

    change url mongo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
+// require("dotenv").config();
 const cors = require('cors'); //requires the cors module, which is used to config server.headers
-const mongoURI = process.env.MONGO_URI;
+// const mongoURI = process.env.MONGO_URI;
 
 const express = require("express");  // requires the Express library, which is used to create a web server.
 const mongoose = require("mongoose") // requires the Mongoose library  -- which is used to connect to a MongoDB database.
@@ -21,9 +21,15 @@ app.use(express.json()); //Used to parse incoming or transform requests with JSO
 
 app.use(cors());
 
+const use="todolist";
+const password='1234';
+const dbname='todolist';
+
+const url = `mongodb+srv://${use}:${password}@bd.y5qzq9a.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
 //connect to mongo atlas(cluster) > todo(db) > todos (collection)
 mongoose.set("strictQuery", false);
-mongoose.connect(mongoURI,connectionOptions).then(()=>console.log("Conexion exitosa")).catch((err)=> console.error(err));
+mongoose.connect(url,connectionOptions).then(()=>console.log("Conexion exitosa")).catch((err)=> console.error(err));
 
 app.use("/App",todoRoutes );
 
